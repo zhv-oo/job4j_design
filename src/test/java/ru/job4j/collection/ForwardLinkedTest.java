22a62346e0c862aae880d1c45f372071d9e6fae6
zhv-oo <zhitenev.oleg@yandex.ru>
2021-07-01 23:33:13 +0700

    3. Удалить head в односвязном списке. [#51424 #70147]

---
+++ src/test/java/ru/job4j/collection/ForwardLinkedTest.java
@@ -0,0 +1,48 @@
+package ru.job4j.collection;
+
+
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class ForwardLinkedTest {
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenDeleteFirst() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.add(1);
+        linked.deleteFirst();
+        linked.iterator().next();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenDeleteEmptyLinked() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.deleteFirst();
+    }
+
+    @Test
+    public void whenMultiDelete() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.add(1);
+        linked.add(2);
+        assertThat(linked.deleteFirst(), is(1));
+        Iterator<Integer> it = linked.iterator();
+        assertThat(it.next(), is(2));
+    }
+
+    @Test
+    public void whenDeleteTwoFirstElem() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.add(1);
+        linked.add(2);
+        assertThat(linked.deleteFirst(), is(1));
+        assertThat(linked.deleteFirst(), is(2));
+        Iterator<Integer> it = linked.iterator();
+        assertFalse(it.hasNext());
+    }
+}
