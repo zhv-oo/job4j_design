09fe5109189634e59f4c17921f7cf010b5d008f5
zhv-oo <zhitenev.oleg@yandex.ru>
2021-07-04 17:01:54 +0700

    3. Переопределить только hashCode

--- src/main/java/ru/job4j/collection/map/User.java
+++ src/main/java/ru/job4j/collection/map/User.java
@@ -1,6 +1,6 @@
 package ru.job4j.collection.map;
 
-import java.util.Calendar;
+import java.util.*;
 
 public class User {
     private String name;
@@ -11,5 +11,25 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+
+    public static void main(String[] args) {
+        User user = new User("Ivan", 2,
+                new GregorianCalendar(1980, 11, 11, 10, 10, 10));
+        User userTwo = new User("Ivan", 2,
+                new GregorianCalendar(1980, 11, 11, 10, 10, 10));
+        Map<User, Object> test = new HashMap<>();
+        test.put(user, new Object());
+        test.put(userTwo, new Object());
+        for (User userT : test.keySet()) {
+            System.out.println(userT);
+            System.out.println(userT.hashCode());
+        }
+        System.out.println(test);
     }
 }
