03ee6d8e67c12541fc735e833c518080aef514e8
zhv-oo <zhitenev.oleg@yandex.ru>
2021-07-08 17:43:43 +0700

    0.1. FileOutputStream.

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,18 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            for (int i = 1; i < 10; i++) {
+                for (int j = 1; j < 10; j++) {
+                    out.write(String.valueOf(j * i).getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
