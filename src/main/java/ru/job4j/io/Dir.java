3da1fc12d79a83f2fef099f446b866c8dd7b4d8f
zhv-oo <zhitenev.oleg@yandex.ru>
2021-07-10 10:13:53 +0700

    4.0. File [#252491]

---
+++ src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,19 @@
+package ru.job4j.io;
+
+import java.io.File;
+import java.util.Objects;
+
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("/Users/zhv/IdeaProjects/");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        for (File subfile : Objects.requireNonNull(file.listFiles())) {
+            System.out.println(subfile.getName() + " : " + subfile.length());
+        }
+    }
+}
