9c9d95cb5bd029a258b24de243589c2e4763090e
zhv-oo <zhitenev.oleg@yandex.ru>
2021-07-10 10:01:53 +0700

    2. Анализ доступности сервера. [#859 #175691]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -1,32 +1,26 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.*;
-import java.util.stream.Collectors;
 
 public class Analizy {
     public void unavailable(String source, String target) {
-        List<String> tmpList = new ArrayList<>();
-        try (BufferedReader line = new BufferedReader(new FileReader(source))) {
-            tmpList = line.lines().collect(Collectors.toList());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        try (PrintWriter out = new PrintWriter(
-                new BufferedOutputStream(
-                        new FileOutputStream(target)
-                ))) {
-            int chk = 0;
-            for (String str : tmpList) {
-               String[] line = str.split(" ");
-               int status = Integer.parseInt(line[0]);
-               if (status >= 400 && chk == 0) {
-                   out.write(line[1] + ";");
-               } else if (status < 400 && chk == 1) {
-                   out.write(line[1] + ";");
-                   out.write(System.lineSeparator());
-               }
-                chk = status >= 400 ? 1 : 0;
+        try (BufferedReader in = new BufferedReader(new FileReader(source))) {
+            try (PrintWriter out = new PrintWriter(
+                    new BufferedOutputStream(
+                            new FileOutputStream(target)
+                    ))) {
+                int chk = 0;
+                for (String line = in.readLine(); line != null; line = in.readLine()) {
+                    String[] tmp = line.split(" ");
+                    int status = Integer.parseInt(tmp[0]);
+                    if (status >= 400 && chk == 0) {
+                        out.write(tmp[1] + ";");
+                    } else if (status < 400 && chk == 1) {
+                        out.write(tmp[1] + ";");
+                        out.write(System.lineSeparator());
+                    }
+                    chk = status >= 400 ? 1 : 0;
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -34,6 +28,6 @@ public class Analizy {
     }
 
     public static void main(String[] args) {
-      //  Analizy.unavailable("./data/server.log", "./data/unavailable.csv");
+        //  Analizy.unavailable("./data/server.log", "./data/unavailable.csv");
     }
 }
