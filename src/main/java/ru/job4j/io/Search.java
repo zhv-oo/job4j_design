fd5d087c9c8f4acee694eba0ff8c34e97adf0391
zhv-oo <zhitenev.oleg@yandex.ru>
2021-07-11 21:13:13 +0700

    4.1. Сканирование файловой системы. [#106929]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -12,7 +12,7 @@ import static java.nio.file.FileVisitResult.CONTINUE;
 public class Search {
     public static void main(String[] args) throws IOException {
         Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith("java")).forEach(System.out::println);
+        search(start, p -> p.toFile().getName().endsWith("js")).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
