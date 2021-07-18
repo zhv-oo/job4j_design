700508dc7238d6932dd9a425955fc6ad467bf846
zhv-oo <zhitenev.oleg@yandex.ru>
2021-07-18 15:03:24 +0700

    1. Бот [#7921 #178805]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -12,6 +12,7 @@ public class EchoServer {
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
                         String[] tmp = str.split("=|, | ");
                         if (tmp.length > 2) {
