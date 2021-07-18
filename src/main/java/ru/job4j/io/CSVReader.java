b399f9860196f127fd94a036c8dbc053ee0efda2
zhv-oo <zhitenev.oleg@yandex.ru>
2021-07-18 14:46:26 +0700

    7. Scanner [#504791 #178480]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -1,38 +1,64 @@
 package ru.job4j.io;
 
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class CSVReader {
-    private final List<String> lineSource =
-            new ArrayList<>(List.of("name", "age", "birthDate", "education", "children"));
+    private List<String> lineSource = new ArrayList<>();
+    private List<Integer> indexColumn = new ArrayList<>();
+    private final Path outPath = Path.of("./data/csvOut.txt");
+    private final List<String> outList = new ArrayList<>();
 
     public void readCsv(Path pathIn, String delim, String out, String filter) {
-        String[] strings = filter.split(",");
-        List<Integer> indexColumn = new ArrayList<>();
-        for (String string : strings) {
-            indexColumn.add(lineSource.indexOf(string));
-        }
-        try (var scanner = new Scanner(pathIn.toFile()).useDelimiter(delim + "|, |\\n")) {
-            if (out.equals("stdout")) {
+        try (var scanner = new Scanner(pathIn.toFile()).useDelimiter(delim)) {
+            if (scanner.hasNext()) {
+                this.lineSource = List.of(filter.split(","));
+                String[] tmps = scanner.nextLine().split(delim);
+                int j = 0;
+                for (String string : tmps) {
+                    if (lineSource.contains(string)) {
+                        this.indexColumn.add(j);
+                    }
+                    j++;
+                }
                 int i = 0;
                 while (scanner.hasNext()) {
-                    String tmp = scanner.next();
-                    if (i == lineSource.size()) {
-                        i = 0;
-                        System.out.println();
+                    String[] tmp = scanner.nextLine().split(delim);
+                    for (String s : tmp) {
+                        if (i == tmp.length) {
+                            i = 0;
+                            outList.add(System.lineSeparator());
+                        }
+                        if (indexColumn.contains(i)) {
+                            outList.add(s);
+                            outList.add(" ");
+                        }
+                        i++;
                     }
-                    if (indexColumn.contains(i)) {
-                        System.out.print(tmp);
-                        System.out.print(" ");
-                    }
-                    i++;
                 }
             }
+            if (out.equals("stdout")) {
+                outList.forEach(System.out::print);
+            } else {
+                writeFile(outList);
+            }
         } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void writeFile(List<String> out) {
+        try (PrintWriter pw = new PrintWriter(
+                new FileWriter(outPath.toFile(), Charset.forName("WINDOWS-1251"), true))) {
+            out.forEach(pw::print);
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
