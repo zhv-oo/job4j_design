6e48ffed1b63ece26a3a89e6e8bb5d2c997325be
zhv-oo <zhitenev.oleg@yandex.ru>
2021-07-23 23:39:02 +0700

    2. Поиск файлов по критерию [#783 #180554]

--- src/main/java/ru/job4j/finder/Finder.java
+++ src/main/java/ru/job4j/finder/Finder.java
@@ -2,26 +2,18 @@ package ru.job4j.finder;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.job4j.io.Search;
 import ru.job4j.io.UsageLog4j;
-
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
 
-import static java.nio.file.FileVisitResult.CONTINUE;
+import static ru.job4j.finder.Search.search;
 
 public class Finder {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
-    private String outPath;
-    private String startDir;
+    private final String outPath;
     @SuppressWarnings("checkstyle:ConstantName")
     private static final List<String> HL = List.of(
             "Для запуска в консоли введите java -jar finder.jar -d=c:/ -n=*.txt -t=mask -o=log.txt",
@@ -33,70 +25,38 @@ public class Finder {
              );
     private List<Path> resultList = new ArrayList<>();
 
-    public Finder(String startDir, String outPath) {
-        this.startDir = startDir;
+    public Finder(String outPath) {
         this.outPath = outPath;
     }
 
-    public List<Path> getResultList() {
-        return resultList;
-    }
-
-    private static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
-        Search.SearchFiles searcher = new Search.SearchFiles(condition);
-        Files.walkFileTree(root, searcher);
-        return searcher.getPaths();
-    }
-
-    public static class SearchFiles
-            extends SimpleFileVisitor<Path> {
-        private final Predicate<Path> condition;
-        private final List<Path> list = new ArrayList<>();
-
-
-        public SearchFiles(Predicate<Path> condition) {
-            this.condition = condition;
-        }
-
-        public List<Path> getPaths() {
-            return list;
-        }
-
-        @Override
-        public FileVisitResult visitFile(Path file,
-                                         BasicFileAttributes attr) {
-            if (condition.test(file)) {
-                list.add(file);
-            }
-            return CONTINUE;
-        }
-    }
-
-    private void writeFile(List<Path> out) {
+    private void writeFile() {
         try (PrintWriter pw = new PrintWriter(
                 new FileWriter(outPath, StandardCharsets.UTF_8, true))) {
-            out.forEach(pw::println);
+            resultList.forEach(pw::println);
         } catch (IOException e) {
             LOG.error("Exception:", e);
+        }
+    }
+
+    private void searcher(String searchType, String startDir, String tag) throws IOException {
+        if (searchType.equals("mask")) {
+            resultList = search(Path.of(startDir),
+                    p -> p.toFile().getName().endsWith(tag.replaceFirst("\\*", "")));
+        } else if (searchType.equals("name")) {
+            resultList = search(Path.of(startDir),
+                    p -> p.toFile().getName().equals(tag));
+        } else {
+            resultList = search(Path.of(startDir),
+                    p -> p.toFile().getName().contains(tag));
         }
     }
 
     public static void main(String[] args) {
         try {
             GetArgs getArgs = GetArgs.of(args);
-            Finder finder = new Finder(getArgs.get("d"), getArgs.get("o"));
-            String typeSearch = getArgs.get("t");
-            if (typeSearch.equals("mask")) {
-                finder.resultList = search(Path.of(finder.startDir),
-                        p -> p.toFile().getName().endsWith(getArgs.get("n").replaceFirst("\\*", "")));
-            } else if (typeSearch.equals("name")) {
-                finder.resultList = search(Path.of(finder.startDir),
-                        p -> p.toFile().getName().startsWith(getArgs.get("n")));
-            } else {
-                finder.resultList = search(Path.of(finder.startDir),
-                        p -> p.toFile().getName().contains(getArgs.get("n")));
-            }
-            finder.writeFile(finder.resultList);
+            Finder finder = new Finder(getArgs.get("o"));
+            finder.searcher(getArgs.get("t"), getArgs.get("d"), getArgs.get("n"));
+            finder.writeFile();
         } catch (IllegalArgumentException | IOException e) {
             HL.forEach(System.out::println);
         }
---
+++ src/main/java/ru/job4j/finder/Search.java
@@ -0,0 +1,54 @@
+package ru.job4j.finder;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static java.nio.file.FileVisitResult.CONTINUE;
+
+
+public class Search {
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar search.jar ROOT_FOLDER.");
+        }
+        if (args.length == 1) {
+            throw new IllegalArgumentException("Enter file extension, Usage java -jar search.jar ROOT_FOLDER FILE_EXTENSION.");
+        }
+        Path start = Paths.get(args[0]);
+        String fileEx = args[1];
+        search(start, p -> p.toFile().getName().endsWith(fileEx)).forEach(System.out::println);
+    }
+
+    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
+        SearchFiles searcher = new SearchFiles(condition);
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+
+    public static class SearchFiles
+            extends SimpleFileVisitor<Path> {
+        private final Predicate<Path> condition;
+        private final List<Path> list = new ArrayList<>();
+
+        public SearchFiles(Predicate<Path> condition) {
+            this.condition = condition;
+        }
+
+        public List<Path> getPaths() {
+            return list;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file,
+                                         BasicFileAttributes attr) {
+            if (condition.test(file)) {
+                list.add(file);
+            }
+            return CONTINUE;
+        }
+    }
+}
