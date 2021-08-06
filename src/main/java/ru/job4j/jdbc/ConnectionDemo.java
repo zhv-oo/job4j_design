bdadba7092871dd0afec4d27e5e806672218a64a
zhv-oo <zhitenev.oleg@yandex.ru>
2021-08-06 21:45:58 +0700

    0. JDBC [#6863]

---
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -0,0 +1,23 @@
+package ru.job4j.jdbc;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import ru.job4j.io.Config;
+
+public class ConnectionDemo {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        Config config = new Config("app.properties");
+        config.load();
+        String url = config.value("url");
+        String login = config.value("username");
+        String password = config.value("password");
+        try (Connection connection = DriverManager.getConnection(url, login, password)) {
+            DatabaseMetaData metaData = connection.getMetaData();
+            System.out.println(metaData.getUserName());
+            System.out.println(metaData.getURL());
+        }
+    }
+}
