3c6423ca99596c2d2066bf26930b1e1dc472a60a
zhv-oo <zhitenev.oleg@yandex.ru>
2021-11-10 11:55:43 +0700

    0. Принцип открытости закрытости [#4914 #226862]

---
+++ src/main/java/ru/job4j/ocp/Users.java
@@ -0,0 +1,52 @@
+package ru.job4j.ocp;
+
+/**
+ * \* User: zhv
+ * \* Date: 10.11.2021
+ * \* Time: 11:11
+ * \* Description: Примеры нарушения принципа OCP.
+ * \
+ */
+
+public class Users {
+
+    public String name;
+
+    /**
+     * Первый пример это жесткая связка с другим класом. Вызов одного класса в другом.
+     */
+
+    static class Car {
+
+    }
+
+    static class WashCar {
+        void washACar() {
+            Car car = new Car();
+        }
+    }
+
+    /**
+     * Второй пример это тоже привязка к другому классу. А не к абстрактному.
+     */
+
+    static class Person {
+        void getName(User user) {
+            System.out.println(user.name);
+        }
+    }
+
+    /**
+     * Третий пример привязка к возвращаемым типам.
+     */
+
+    static class Toy {
+
+    }
+
+    static class ToyShop {
+        Toy getAnimal() {
+            return new Toy();
+        }
+    }
+}
