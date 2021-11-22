e27dfae87a6b0e6c173dac6b0e50ae37353830fc
zhv-oo <zhitenev.oleg@yandex.ru>
2021-11-22 13:59:02 +0700

    2. Парковка машин [#853]
    Signed-off-by: Zhitenev <oleg@openres.ru>

---
+++ src/main/java/ru/job4j/lsp/parking/Car.java
@@ -0,0 +1,12 @@
+package ru.job4j.lsp.parking;
+
+/**
+ * \* User: zhv
+ * \* Date: 22.11.2021
+ * \* Time: 13:11
+ * \* Description: Интерфейс для реализации объектов типа Автомобиль разного размера.
+ * \
+ */
+public interface Car {
+    int getCarSize();
+}
---
+++ src/main/java/ru/job4j/lsp/parking/CarParking.java
@@ -0,0 +1,12 @@
+package ru.job4j.lsp.parking;
+
+/**
+ * \* User: zhv
+ * \* Date: 22.11.2021
+ * \* Time: 13:11
+ * \* Description: Интерфейс для реализации механизма постановки на парковку.
+ * \
+ */
+interface CarParking {
+    boolean parkingCar(Car car, Park park);
+}
